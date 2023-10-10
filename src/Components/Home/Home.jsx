0e69cd0241f68fb6a49b87c985477d07d0f802b7
manojvs17 <manojmendy5@gmail.com>
2023-10-10 14:21:29 +0530

    text color changed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
           <p className='font-poppins text-[1.2em] text-center md:text-[1.6em]'>This is a website that can help you to encode or decode your infromation</p>
         </div>
     </div>
-    <h1 className='text-center pt-10 text-[1.3em] pb-4 font-bold'>Have a Try By Clicking Encrypt Button below</h1>
+    <h1 className='text-center text-[#F73D93] pt-10 text-[1.3em] pb-4 font-bold'>Have a Try By Clicking Encrypt Button below</h1>
       <ul className='flex justify-evenly text-[blue] text-[1em] font-bold'>
         <li className='p-2 text-center btn font-poppins'><Link to="/encrypt">Encrypt</Link></li>
         <li className='p-2 text-center btn font-poppins'><Link to="/decrypt">Decrypt</Link></li>
